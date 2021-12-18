c0c2cb13579f89593b95d0ef4a96da1057aa149a
ddau-stack <dudau0924222@gmail.com>
2021-12-18 15:33:42 -0500

    finish single word encryption

--- src/Cipher.py
+++ src/Cipher.py
@@ -1,6 +1,5 @@
 import unittest
 from FileParser import FileParser as fp
-from Encryptable import Encryptable
 
 class CipherTest(unittest.TestCase):
     def testConstructor(self):
@@ -27,41 +26,43 @@ class CipherTest(unittest.TestCase):
         else:
             self.assertEqual(False, True)
 
+    def testGetResult(self):
+        testCipher = Cipher("testFiles/Result.txt", 0)
+        result = testCipher.getResult()
+        expected = "Result"
+        self.assertEqual(expected, result)
+
     def testSingleWordEncrypt(self):
-        testCipher = Cipher("testFiles/Single.txt")
-        testCipher.encrypt()
-        file = open("testFiles", "r")
-        result = file.readLines()
-        expected = [Yrug]
-        assertEqual(expected, result)
+        testCipher = Cipher("testFiles/Single.txt", 3)
+        testCipher.encrypt(testCipher.fileContents, 3)
+        result = testCipher.getResult()
+        expected = "Zrug"
+        self.assertEqual(expected, result)
 
 class Cipher:
+    def encrypt(self, text, shift):
+        result = ""
+        for i in range(len(text)):
+            char = text[i]      
+            if (char.isupper()):
+                result += chr((ord(char) + shift-65) % 26 + 65)
+            elif char.islower():
+                result += chr((ord(char) + shift-97) % 26 + 97)
+            else:
+                result += char
+        return result
+
     def __init__(self, *args):
         if len(args) != 2:
             raise ValueError
         else:
-            parser = fp(args[0])
-            fileContents = parser.getContents()
-            for i in range(0, len(contents)):
-                encrypt(fileContents[i], args[1])
+            self.parser = fp(args[0])
+            self.fileContents = self.parser.getContents()
+            self.result = self.encrypt(self.fileContents, args[1])
 
-    def encrypt(text, shift):
-        result = ""
-        # transverse the plain text
-        for i in range(len(text)):
-            char = text[i]
-        # Encrypt uppercase characters in plain text
-      
-        if (char.isupper()):
-            result += chr((ord(char) + shift-65) % 26 + 65)
-        # Encrypt lowercase characters in plain text
-        elif char.islower():
-            result += chr((ord(char) + shift-97) % 26 + 97)
-        else:
-            result += char
-        return result
-
-
+    def getResult(self):
+        print("Returning: "+self.result)
+        return self.result
 
 if __name__ == '__main__':
     unittest.main() 
