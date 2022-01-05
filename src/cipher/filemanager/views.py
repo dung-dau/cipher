9be46b86495a34216ef82a04ef37e6a4ae91479c
ddau-stack <dudau0924222@gmail.com>
2022-01-04 19:46:10 -0500

    file page now retrieves information from the database correctly

--- src/cipher/filemanager/views.py
+++ src/cipher/filemanager/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from .models import File
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the home page.")
 
 def file(request, name):
-	return HttpResponse(name)
+	matched_file = File.objects.filter(name__startswith=name).first()
+
+	return HttpResponse(matched_file.contents)
