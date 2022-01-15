5c86ce2c0e7a1b06246b1018e1ec28bb648ce5a6
ddau-stack <dudau0924222@gmail.com>
2022-01-14 22:50:59 -0500

    add basic file uploading form functionality

--- src/cipher/filemanager/urls.py
+++ src/cipher/filemanager/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<slug>/', views.FileView.as_view(), name='file'),
+    path('handleFile', views.handleFile, name='handleFile'),
 ]
