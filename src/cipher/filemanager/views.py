0bf54effb885fe56555ae09323f7826cbb3992f3
Dung Dau <dudau0924222@gmail.com>
2022-01-24 11:31:33 -0500

    remove the forms.py file

--- src/cipher/filemanager/views.py
+++ src/cipher/filemanager/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import generic
-from django.views.generic import DetailView
 from django.urls import reverse
 
-from .cipher_api.parser import Parser
+from .cipher_api.parser.parser import Parser
 
 from .models import File
-from .forms import DeleteFileForm, UploadFileForm
-# from .cipherAPI.Cipher import Cipher
-from django.core.files.storage import FileSystemStorage
-import os
 
 class IndexView(generic.ListView):
     template_name = 'filemanager/index.html'
@@ -26,11 +20,9 @@ class FileView(generic.DetailView):
 
 def handleFile(request):
     file_name = request.FILES['filename'].name
-    file_contents = request.FILES['filename'].read()
-    # file_to_encrypt = Cipher(request.FILES['filename'].temporary_file_path, 3)
-    textParser = Parser(file_contents, 3)
-    print(textParser.getEncryptedText())
-    new_file = File(name=file_name, contents=file_contents.decode('utf-8'))
+    file_contents = request.FILES['filename'].read().decode('utf-8')
+    textParser = Parser(str(file_contents), 3)
+    new_file = File(name=file_name, contents=textParser.getEncryptedText())
     new_file.save()
     return HttpResponseRedirect(reverse('filemanager:index'))
 
